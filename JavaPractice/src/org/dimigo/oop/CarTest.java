022e14d21270fed2b87dcf6a589167bf42f2b8a9
user1 <user1@home.local>
2015-04-17 23:28:19 +0900

    7-2

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -21,33 +21,27 @@ public class CarTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Car car=new Car();
-		
-		car.setCompany("현대자동차");
-		car.setModel("제네시스");
-		car.setColor("검정색");
-		car.setMaxSpeed(225);
-		car.setPrice(50000000);
-		
+		Car2 car=new Car2("현대자동차","제네시스","검정색",225,50000000);
 		System.out.println("<<자동차 목록>>");
 		System.out.println("제조사명: "+car.getCompany());
 		System.out.println("모델명: "+car.getModel());
 		System.out.println("색상: "+car.getColor());
 		System.out.println("최대속도: "+car.getMaxSpeed()+"km");
 		System.out.println("가격: "+String.format("%,d",car.getPrice())+"원");
+		System.out.println();
 		
 		car.setCompany("기아자동차");
 		car.setModel("K7");
 		car.setColor("흰색");
 		car.setMaxSpeed(246);
-		car.setPrice(40000000);
+		car.setPrice(4000000);
 		
-		System.out.println();
 		System.out.println("제조사명: "+car.getCompany());
 		System.out.println("모델명: "+car.getModel());
 		System.out.println("색상: "+car.getColor());
 		System.out.println("최대속도: "+car.getMaxSpeed()+"km");
 		System.out.println("가격: "+String.format("%,d",car.getPrice())+"원");
+		System.out.println();
 		
 		car.setCompany("삼성자동차");
 		car.setModel("SM7");
@@ -55,12 +49,12 @@ public class CarTest {
 		car.setMaxSpeed(200);
 		car.setPrice(38000000);
 		
-		System.out.println();
 		System.out.println("제조사명: "+car.getCompany());
 		System.out.println("모델명: "+car.getModel());
 		System.out.println("색상: "+car.getColor());
 		System.out.println("최대속도: "+car.getMaxSpeed()+"km");
 		System.out.println("가격: "+String.format("%,d",car.getPrice())+"원");
+		System.out.println();
 
 	}
 
